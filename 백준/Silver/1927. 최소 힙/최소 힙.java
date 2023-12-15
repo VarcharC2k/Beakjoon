e8a73a2eb559f1f2e4f548ccad17c4070095f1fd
VarcharC2k <rudrms950@naver.com>
2023-12-15 12:13:50 +0900

    [Silver II] Title: 최소 힙, Time: 336 ms, Memory: 26180 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+class Main {
+
+    public static int n;
+    public static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> d = new PriorityQueue<>();
+        sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            if (m == 0) {
+                if (d.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(d.poll()).append("\n");
+                }
+            } else {
+                d.add(m);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
