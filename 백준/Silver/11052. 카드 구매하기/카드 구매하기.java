4424c7be83bc6f356693e663e9eeb09084059c1a
VarcharC2k <rudrms950@naver.com>
2023-12-20 10:42:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static int n;
+    public static int[] arr;
+    public static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n+1];
+        dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
